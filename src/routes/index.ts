258e69d99d3d2db5f0c95f41aee2c765ab769d2f
Glenn LeBlanc <g.leblanc135@gmail.com>
2020-08-15 18:10:33 -0700

    Uniform try/catch usage

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,17 +5,18 @@ import { User } from '../models/user'
 const router: express.Router = express.Router();
 
 router.get('/', async (req, res) => {
-  try {
-      await sequelize.authenticate();
-      console.log('Connection has been established successfully.');
-    } catch (error) {
-      console.error('Unable to connect to the database:', error);
-  }
-  var users = User.findAll()
-      .catch(() => {
-          console.error('badbad');
-      });
-  res.render('index', users);
+    sequelize.authenticate()
+        .then(() => {
+            console.log('Connection has been established successfully.');
+        })
+        .catch((error) => {
+            console.error('Unable to connect to the database:', error);
+        });
+    var users = User.findAll()
+        .catch(() => {
+            console.error('badbad');
+        });
+    res.render('index', users);
 });
 
 router.get('/login', (req, res) => {
